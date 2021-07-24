95921b7577e30b5412dadedee45b87fd3ac290c7
Dipesh Paul <dipeshpal17@gmail.com>
2021-07-24 18:37:27 +0530

    Update api

--- op.py
+++ op.py
@@ -1,61 +1,42 @@
 from pydantic import BaseModel, Field
 from transformers import pipeline
 
-MODEL_NAME = "distilgpt2"
+from opyrator.components import outputs
+
+MODEL_NAME = "deepset/roberta-base-squad2"
 # Load model
-nlp = pipeline("text-generation", model=MODEL_NAME, tokenizer=MODEL_NAME)
+nlp = pipeline("question-answering", model=MODEL_NAME, tokenizer=MODEL_NAME)
 
 
-class TextGenerationInput(BaseModel):
-    text: str = Field(
+class QuestionAnsweringInput(BaseModel):
+    context: str = Field(
         ...,
-        title="Text Input",
-        description="The input text to use as basis to generate text.",
+        description="A string that contains the answer to the question.",
+        example="My name is Sarah and I live in London",
         max_length=1000,
     )
-    temperature: float = Field(
-        1.0,
-        gt=0.0,
-        multiple_of=0.001,
-        description="The value used to module the next token probabilities.",
+    question: str = Field(
+        ...,
+        description="The question as a string that has an answer within context.",
+        example="Where do I live?",
+        max_length=140,
     )
-    max_length: int = Field(
-        30,
-        ge=5,
-        le=100,
-        description="The maximum length of the sequence to be generated.",
-    )
-    repetition_penalty: float = Field(
-        1.0,
-        ge=0.0,
-        le=1.0,
-        description="The parameter for repetition penalty. 1.0 means no penalty.",
-    )
-    top_k: int = Field(
-        50,
-        ge=0,
-        description="The number of highest probability vocabulary tokens to keep for top-k-filtering.",
-    )
-    do_sample: bool = Field(
-        False,
-        description="Whether or not to use sampling ; use greedy decoding otherwise.",
+    number_of_answers: int = Field(
+        2, ge=1, description="The number of answers to return"
     )
 
 
-class TextGenerationOutput(BaseModel):
-    generated_text: str = Field(...)
+def question_answering(input: QuestionAnsweringInput) -> outputs.ClassificationOutput:
+    """Question answering based on a text used as context."""
 
-
-def generate_text(input: TextGenerationInput) -> TextGenerationOutput:
-    """Generate text based on a given prompt."""
-
-    res = nlp(
-        input.text,
-        temperature=input.temperature,
-        max_length=input.max_length,
-        repetition_penalty=input.repetition_penalty,
-        top_k=input.top_k,
+    results = nlp(
+        {"question": input.question, "context": input.context},
+        topk=input.number_of_answers,
     )
-    return TextGenerationOutput(generated_text=res[0]["generated_text"])
 
-
+    return outputs.ClassificationOutput(
+        __root__=[
+            outputs.ScoredLabel(label=result["answer"], score=result["score"])
+            for result in results
+        ]
+    )

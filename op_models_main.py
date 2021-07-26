04c1b9cced63a53bc596eeb7de52fbe3d321a9e1
Dipesh Paul <dipeshpal17@gmail.com>
2021-07-26 10:54:46 +0530

    update

--- op_models_main.py
+++ op_models_main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from transformers import pipeline
-from pyngrok import ngrok
 import requests
 
 
@@ -25,7 +24,7 @@ if not hasattr(st, 'already_started_server'):
 
     st.write('''
             The first time this script executes it will run forever because it's
-            running a Opyrator server.
+            running a FastAPI server.
 
             Just close this browser tab and open a new one to see your Streamlit
             app.
@@ -33,8 +32,6 @@ if not hasattr(st, 'already_started_server'):
 
     import os
 
-
-    from joblib import Parallel, delayed
     import os
     from pyngrok import ngrok
 
@@ -47,35 +44,17 @@ if not hasattr(st, 'already_started_server'):
         os.system("opyrator launch-ui op_asr:separate_audio --port 8080")
 
 
-    def servers():
-        endpoint2 = ngrok.connect(8000).public_url
-        return endpoint2
-
-
-    models_running = []
-    models_running.append(delayed(fastapi_models)())
-    models_running.append(delayed(opyrator_models)())
-
-    server_list = []
-    server_list.append(delayed(servers)())
-
     endpoint = ngrok.connect(8080).public_url
-    endpoint2 = Parallel(n_jobs=-1, verbose=11)(server_list)
-    print(endpoint, endpoint2[0])
-
-    endpoints = f"opyrator_server: {endpoint} | fastapi_server: {endpoint2[0]}"
-
     status = requests.get(
         f'https://jarvis-ai-api.herokuapp.com/update_api_endpoint/?username=dipeshpal&token={st.secrets["token"]}&endpoint={endpoint}')
-    print("endpoint------------------------------", endpoints)
-
-    returned_data = Parallel(n_jobs=-1, verbose=11)(models_running)
+    print("endpoint------------------------------", endpoint)
+    opyrator_models()
 
 
 # this is the main function in which we define our webpage
 def main():
     # front end elements of the web page
-    st.title('Transformers Q and A Demo')
+    st.title('Transformers Q and A Demo [OP]')
 
     # following lines create boxes in which user can enter data required to make prediction
     c = st.text_area("Context", "My name is Robert")
